0c5e3d17144297c7c2c36070c4b850722cdbb0f3
DAChernikov <129949710+DAChernikov@users.noreply.github.com>
2025-01-22 16:03:29 +0300

    Add data-module of Telegram

---
+++ data/users_data.py
@@ -0,0 +1,26 @@
+"""
+Глобальный словарь пользователей.
+Помимо основных полей, здесь теперь
+храним историю логов, чтобы строить
+графики по временной шкале.
+"""
+users = {
+    # user_id: {
+    #     "weight": ...,
+    #     "height": ...,
+    #     "age": ...,
+    #     "activity": ...,
+    #     "city": ...,
+    #     "water_goal": ...,
+    #     "calorie_goal": ...,
+    #     "logged_water": 0,
+    #     "logged_calories": 0,
+    #     "burned_calories": 0,
+    #     "log_history": [
+    #         {"timestamp": datetime, "type": "water", "amount": 300},
+    #         {"timestamp": datetime, "type": "food", "kcal": 120},
+    #         {"timestamp": datetime, "type": "workout", "kcal": 300},
+    #         ...
+    #     ]
+    # }
+}
